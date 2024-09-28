f682ef8d2c50ce995945d96fee3392ac6d02ebff
tanzeelabibi <peersab862002@gmail.com>
2024-09-29 00:36:05 +0500

    updated

---
+++ src/components/header.tsx
@@ -0,0 +1,16 @@
+export default function Header(){
+    return(
+        <div>
+        <header>
+            
+           <h1 className="header"><b>Tanzeela Syeda</b></h1>
+
+           <ul className="header-buttons">
+            <li>Home</li>
+            <li>About-us</li>
+            <li>Contact-us</li>
+            </ul>
+        </header>
+        </div>
+    )
+}
